61d98ace478633c05f0471f042143352b72c68ef
Lucass2160 <lucas-cali@hotmail.com>
2023-04-25 02:08:22 -0300

    avanzando

--- api/src/models/Diets.js
+++ api/src/models/Diets.js
@@ -3,28 +3,12 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("recipe", {
+  sequelize.define("diets", {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.BLOB,
-      allowNull: false,
-    },
-    summary: {
-      type: DataTypes.STRING(1000),
-      allowNull: false,
-    },
-    healtScore: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    intrunction: {
       type: DataTypes.STRING,
       allowNull: false,
     },
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const recipesMiddleware = require("./middleware/recipe.js");
+const dietsMiddleware = require("./middleware/diets.js");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/recipes", recipesMiddleware);
+router.use("/recipes", dietsMiddleware);
 
 module.exports = router;
