e2a4c151761a90864c016dec458f7df6bf3e6fc2
Lucass2160 <lucas-cali@hotmail.com>
2023-04-27 17:21:28 -0300

    cambios

--- api/src/controllers/recipe_name_controllers.js
+++ api/src/controllers/recipe_name_controllers.js
@@ -12,14 +12,19 @@ recipesModelsName.get("/", async (req, res) => {
       where: { name: { [Op.iLike]: `%${name}%` } },
       include: Diets,
     });
+
+    // Buscar los videojuegos en la API externa
     const response = await axios.get(
       `https://api.spoonacular.com/recipes/search?apiKey=1ca2f2e0158a4007b974f8038badf39c&query=${name}&addRecipeInformation=true`
     );
-    console.log(response);
+
     const recipe = {
-      id: data.id,
-      name: data.title,
-      image: data.image,
+      id: recipeAll.id,
+      name: recipeAll.title,
+      image: recipeAll.image,
+      summary: recipeAll.summary,
+      healScore: recipeAll.healthScore,
+      instruction: recipeAll.instructions,
     };
   } catch (error) {
     res.status(404).json({ error: "pis" });
