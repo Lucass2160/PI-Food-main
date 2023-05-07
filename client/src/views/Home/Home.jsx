e1ac776267d3605f74855e916bb3657ade486c1d
Lucass2160 <lucas-cali@hotmail.com>
2023-05-06 22:45:32 -0300

    cada vez falta menos!

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,25 +1,50 @@
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import { useEffect } from "react";
+import Filter from './Filters/Filter';
+import { useSelector } from "react-redux";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import  {getRecipes}  from "../../redux/actions";
+import { getAllDiet } from "../../redux/actions"
+import { getRecipes } from "../../redux/actions";
 import SearchBar from "../../components/SearchBar/SerachBar";
+import("./home.css");
 
 const Home = () => {
+  const allDiet = useSelector((state) => state.diets);
+  console.log(allDiet, "dietas")  
+  console.log()
 
-  
+  const [order, setOrder] = useState(''); //para guardar los ordenamientos
+  const [socre, setScore] = useState('');
+
   const dispatch = useDispatch();
   useEffect(() => {
-     console.log(getRecipes())
     dispatch(getRecipes());
+    dispatch(getAllDiet())
   }, [dispatch]);
 
+
+
+
   return (
+    
     <>
+      <div className="searchBar">
+        <SearchBar />
+      </div>
+
       
-      <h1>Vista del Home</h1>
-      <SearchBar/>
+
+      <div>
+        <div>
+          <Filter diet={allDiet} setorder={setOrder} setscore={setScore} />
+          
+        </div>
+      </div>
+
+      <h1>Foods</h1>
+
       <CardsContainer />
-      
     </>
   );
 };
